7c87ef309a2ca24d7e94bd907cc49196447034c3
Jake L <jakelovitz@gmail.com>
2019-04-04 13:39:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else
+    formatted_line = []
+    counter = 1
+    katz_deli.each do |i|
+      formatted_line.push("#{counter}. #{i}")
+      counter += 1
+    end
+  end
+  return "The line is currently: #{formatted_line.join(", ")}"
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    first = katz_deli.shift
+    puts "Now serving #{first}."
+  end
+end
