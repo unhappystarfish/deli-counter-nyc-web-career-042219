39db7fc50544ae84a7caeb8ff46dcee8746ba434
Jake L <jakelovitz@gmail.com>
2019-04-04 14:12:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     formatted_line = []
     counter = 1
